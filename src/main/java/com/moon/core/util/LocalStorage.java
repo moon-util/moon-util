61115bd0c9f6910b08fe2affd5408db2b31f55a2
moonsky <xua744531854@163.com>
2020-02-10 13:34:24 +0800

    feat: LocalStorage

---
+++ src/main/java/com/moon/core/util/LocalStorage.java
@@ -0,0 +1,23 @@
+package com.moon.core.util;
+
+import java.io.Serializable;
+
+/**
+ * @author benshaoye
+ */
+final class LocalStorage {
+
+    private LocalStorage() {}
+
+    public static void getItem(String key) {
+
+    }
+
+    public static void removeItem(String key) {
+
+    }
+
+    public static void setItem(String key, Serializable value) {
+
+    }
+}
