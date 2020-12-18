ae3f4e3328cfad815005965804324f17ed916b6e
moonsky <xua744531854@163.com>
2020-12-18 08:02:53 +0800

    refactor: query method case.

--- src/main/java/com/moon/data/jdbc/processing/AccessorProcessor.java
+++ src/main/java/com/moon/data/jdbc/processing/AccessorProcessor.java
@@ -10,6 +10,52 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * interface UsernameGetter {
+ *     String getUsername();
+ *     String getId();
+ * }
+ *
+ * // 返回值可以是 void,boolean,int(等数字类型)
+ * updateById(UsernameGetter getter); // update t_user set username = ? where id = ?
+ * deleteById(UsernameGetter getter); // delete from t_user where id = ?
+ * deleteByIdAndUsername(UsernameGetter getter); // delete from t_user where id = ? and username = ?
+ * deleteByIdOrUsername(UsernameGetter getter); // delete from t_user where id = ? or username = ?
+ *
+ * interface UserInfoGetter extends UsernameGetter {
+ *     String getPassword();
+ * }
+ *
+ * // 返回值可以是 void,boolean,int(等数字类型)
+ * updateById(UserInfoGetter getter); // update t_user set username = ?, password = ? where id = ?
+ * updateUsernameById(UserInfoGetter getter); // update t_user set username = ? where id = ?
+ * updateUsernameById(String id, String username); // update t_user set username = ? where id = ?
+ * updateUsernameById(String username, String id); // update t_user set username = ? where id = ?
+ * updateUsernameById(@P("username") String a, String id); // update t_user set username = ? where id = ?
+ *
+ * // 与参数顺序无关，但名称要一致: update t_user set username = ?, password = ? where id = ?
+ * updateUsernamePasswordById(String username, String password, String id);
+ *
+ * // 查询
+ * UserInfoGetter findById(String id); // select username, password, id from t_user where id = ?
+ * UserInfoGetter selectById(String id); // select username, password, id from t_user where id = ?
+ * UserInfoGetter queryById(String id); // select username, password, id from t_user where id = ?
+ * UserInfoGetter readById(String id); // select username, password, id from t_user where id = ?
+ * UserInfoGetter getById(String id); // select username, password, id from t_user where id = ?
+ *
+ * List&lt;UserInfoGetter&gt; findByUsernameLike(String username); // select username, password, id where username like '%username%'
+ * List&lt;UserInfoGetter&gt; findByUsernameStartsWith(String username); // select username, password, id where username like 'username%'
+ * List&lt;UserInfoGetter&gt; findByUsernameEndsWith(String username); // select username, password, id where username like '%username'
+ * List&lt;UserInfoGetter&gt; findByAgeGt(int age); // select username, password, id where age > ?
+ * List&lt;UserInfoGetter&gt; findByAgeGreatThan(int age); // select username, password, id where age > ?
+ * List&lt;UserInfoGetter&gt; findByAgeLt(int age); // select username, password, id where age < ?
+ * List&lt;UserInfoGetter&gt; findByAgeLessThan(int age); // select username, password, id where age < ?
+ * List&lt;UserInfoGetter&gt; findByAgeLessThanAndUsernameIs(int age, String username); // select username, password, id where age < ? and username = ?
+ * // 此情况所有条件都是 and 和 = : select username, password, id where age =? and username = ? and address = ?
+ * List&lt;UserInfoGetter&gt; findBy(int age, String username, String address);
+ *
+ * insert(UserInfoGetter getter); // insert into t_user (id, username, password) values (?, ?, ?)
+ * insert(String username); // insert into t_user (id, username) values (?, ?)
+ *
  * @author benshaoye
  */
 // @AutoService(Processor.class)
