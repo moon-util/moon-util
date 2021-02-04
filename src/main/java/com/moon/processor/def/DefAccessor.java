15caf3bc3476139e84b0705da8a91ed963fda2c2
moonsky <xua744531854@163.com>
2021-02-05 07:53:45 +0800

    refactor

--- src/main/java/com/moon/processor/def/DefAccessor.java
+++ src/main/java/com/moon/processor/def/DefAccessor.java
@@ -8,8 +8,10 @@ import com.moon.processor.holder.ModelHolder;
 import com.moon.processor.holder.NameHolder;
 import com.moon.processor.holder.PojoHolder;
 import com.moon.processor.utils.Element2;
+import com.moon.processor.utils.Imported;
+import com.moon.processor.utils.Test2;
 
-import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.*;
 
 /**
  * @author benshaoye
@@ -44,16 +46,63 @@ public class DefAccessor implements JavaFileWriteable {
     }
 
     private DeclJavaFile getDeclJavaFile() {
-        DeclJavaFile accessor = DeclJavaFile.classOf(packageName, simpleName);
-        accessor.implement(Element2.getQualifiedName(this.thisAccessor));
+        TypeElement accessor = this.thisAccessor;
+        ElementKind kind = accessor.getKind();
+        if (kind == ElementKind.CLASS && Test2.isNotAny(accessor, Modifier.ABSTRACT)) {
+            if (Imported.isComponent(accessor)) {
+                return null;
+            }
+        }
+        DeclJavaFile impl = DeclJavaFile.classOf(packageName, simpleName);
 
+        if (accessor.getKind() == ElementKind.INTERFACE) {
+            implInterface(impl, accessor);
+        } else if (Test2.isAny(accessor, Modifier.ABSTRACT)) {
+            implAbstract(impl, accessor);
+        } else {
+            implNormal(impl, accessor);
+        }
 
+        return impl;
+    }
 
-        return accessor;
+    private void implInterface(DeclJavaFile impl, TypeElement accessor) {
+        impl.implement(Element2.getQualifiedName(accessor));
+
+        for (Element enclosedElement : accessor.getEnclosedElements()) {
+            if (!isImplCapable(enclosedElement)) {
+                continue;
+            }
+
+        }
+    }
+
+    private void implNormal(DeclJavaFile impl, TypeElement accessor) {
+    }
+
+    private void implAbstract(DeclJavaFile impl, TypeElement accessor) {
+    }
+
+    private static boolean isImplCapable(Element enclosedElement) {
+        // 不是构造器/方法, 跳过
+        if (!(enclosedElement instanceof ExecutableElement)) {
+            return false;
+        }
+        // 静态方法不处理、私有方法不处理
+        if (Test2.isAny(enclosedElement, Modifier.STATIC)) {
+            return false;
+        }
+        if (Test2.isAny(enclosedElement, Modifier.PRIVATE) && Test2.isAny(enclosedElement, Modifier.ABSTRACT)) {
+            throw new IllegalStateException("不能实现私有抽象方法: " + enclosedElement);
+        }
+        return Test2.isAny(enclosedElement, Modifier.ABSTRACT);
     }
 
     @Override
     public void writeJavaFile(JavaWriter writer) {
-        getDeclJavaFile().writeJavaFile(writer);
+        DeclJavaFile file = getDeclJavaFile();
+        if (file != null) {
+            file.writeJavaFile(writer);
+        }
     }
 }
--- src/main/java/com/moon/processor/utils/Imported.java
+++ src/main/java/com/moon/processor/utils/Imported.java
@@ -9,8 +9,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Generated;
+import javax.lang.model.element.TypeElement;
 import java.util.function.Supplier;
 
 /**
@@ -28,6 +30,7 @@ public enum Imported {
     public final static boolean QUALIFIER;
     public final static boolean GENERATED;
     public final static boolean JODA_TIME;
+    public final static boolean SERVICE;
     public final static boolean COMPONENT;
     public final static boolean REPOSITORY;
     public final static boolean SAFEVARARGS;
@@ -43,9 +46,27 @@ public enum Imported {
         JODA_TIME = isNoException(() -> ReadablePeriod.class.toString());
         AUTOWIRED = isNoException(() -> Autowired.class.toString());
         QUALIFIER = isNoException(() -> Qualifier.class.toString());
+        SERVICE = isNoException(() -> Service.class.toString());
         COMPONENT = isNoException(() -> Component.class.toString());
         REPOSITORY = isNoException(() -> Repository.class.toString());
         SAFEVARARGS = isNoException(() -> SafeVarargs.class.toString());
+    }
+
+    public static boolean isImportedComponent() {
+        return COMPONENT || REPOSITORY || SERVICE;
+    }
+
+    public static boolean isComponent(TypeElement element) {
+        if (COMPONENT && element.getAnnotation(Component.class) != null) {
+            return true;
+        }
+        if (REPOSITORY && element.getAnnotation(Repository.class) != null) {
+            return true;
+        }
+        if (SERVICE && element.getAnnotation(Service.class) != null) {
+            return true;
+        }
+        return false;
     }
 
     private static boolean isNoException(Supplier<String> runner) {
